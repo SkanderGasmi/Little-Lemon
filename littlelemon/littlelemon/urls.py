c8e238c332ec9f65ab94918ccc4c9329c7485de2
Skander Gasmi <skandergasmi7@gmail.com>
2023-10-24 17:30:21 +0100

    crud rest apis for menu and booking models

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -16,10 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include  
+from rest_framework import routers
+from restaurant import views
 
  
-urlpatterns = [ 
-    path('admin/', admin.site.urls), 
-    path('restaurant/', include('restaurant.urls'))
+# Create a router and register the UserViewSet
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'booking', views.BookingViewSet)
+router.register(r'menu', views.MenuViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),  # Includes the router's URLs
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
 
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,105 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import *
+from .serializers import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def list(self, request):
+        # Custom logic for listing users
+        pass
+
+    def create(self, request):
+        # Custom logic for creating a user
+        pass
+
+    def retrieve(self, request, pk=None):
+        # Custom logic for retrieving a user by ID
+        pass
+
+    def update(self, request, pk=None):
+        # Custom logic for updating a user by ID
+        pass
+
+    def partial_update(self, request, pk=None):
+        # Custom logic for partially updating a user by ID
+        pass
+
+    def destroy(self, request, pk=None):
+        # Custom logic for deleting a user by ID
+        pass
+
+
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def list(self, request):
+        # Custom logic for listing users
+        pass
+
+    def create(self, request):
+        # Custom logic for creating a user
+        pass
+
+    def retrieve(self, request, pk=None):
+        # Custom logic for retrieving a user by ID
+        pass
+
+    def update(self, request, pk=None):
+        # Custom logic for updating a user by ID
+        pass
+
+    def partial_update(self, request, pk=None):
+        # Custom logic for partially updating a user by ID
+        pass
+
+    def destroy(self, request, pk=None):
+        # Custom logic for deleting a user by ID
+        pass
+
+
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def list(self, request):
+        # Custom logic for listing users
+        pass
+
+    def create(self, request):
+        # Custom logic for creating a user
+        pass
+
+    def retrieve(self, request, pk=None):
+        # Custom logic for retrieving a user by ID
+        pass
+
+    def update(self, request, pk=None):
+        # Custom logic for updating a user by ID
+        pass
+
+    def partial_update(self, request, pk=None):
+        # Custom logic for partially updating a user by ID
+        pass
+
+    def destroy(self, request, pk=None):
+        # Custom logic for deleting a user by ID
+        pass
