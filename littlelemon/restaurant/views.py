2867c2e6bf987aefe9b6fc5aa2ef73b8fa3c3556
Skander Gasmi <skandergasmi7@gmail.com>
2023-10-26 19:40:46 +0100

    update

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -2,19 +2,24 @@ from django.shortcuts import render
 from rest_framework import viewsets, generics
 from .models import *
 from .serializers import *
-from rest_framework.decorators import api_view
-
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated 
 
 
 class UserViewSet(viewsets.ModelViewSet):
    queryset = User.objects.all()
    serializer_class = UserSerializer
-permission_classes = [IsAuthenticated]
+   permission_classes = [IsAuthenticated]
 
-class MenuItemView(generics.ListCreateAPIView):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+
+class MenuItemsView(generics.ListCreateAPIView):
+   permission_classes = [IsAuthenticated]
+   Menu
+   queryset = Menu.objects.all()
+   serializer_class = MenuSerializer
+
+   #queryset = MenuItem.objects.all()
+   #serializer_class = MenuItemSerializer
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
